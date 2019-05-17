9e33a23080aa72a9bc3539407f4240f6213502b3
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-05-17 11:03:14 -0400

    commented code

--- customer.js
+++ customer.js
@@ -1,8 +1,9 @@
+// required packages
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-var data = '';
 require('dotenv').config();
 
+// connect to mysql database --- password saved in .env file to keep secret
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -11,12 +12,14 @@ var connection = mysql.createConnection({
     database: 'storeinventory'
 });
 
+// if connection successful, table is created
 connection.connect(function (err) {
     if (err) throw err;
     console.log('connection successful');
     createTable();
 });
 
+// shows product table from storeinventory sql database
 function createTable() {
     connection.query('SELECT * FROM products', function (err, res) {
         console.table(res);
@@ -24,19 +27,23 @@ function createTable() {
     });
 }
 
+// prompts customer using inquirer
 function promptCustomer(res) {
-    data = res;
     inquirer.prompt([{
         type: 'input',
         name: 'selection',
         message: 'What is the ID of the product you would like to purchase? [ or Q to quit ]'
     }]).then(function (answer) {
 
+        // sets 'validChoice' to false by default
         var validChoice = false;
 
+        // if 'q' is entered, app closes
         if (answer.selection.toUpperCase() === 'Q') {
             process.exit();
         }
+
+        // loops through customer input to see if it matches product from database
         for (var i = 0; i < res.length; i++) {
             if (res[i].item_id === parseInt(answer.selection)) {
                 validChoice = true;
@@ -44,6 +51,8 @@ function promptCustomer(res) {
                 break;
             }
         }
+
+        // if input does not match an available product, table and prompt are shown again
         if (!validChoice) {
             console.log('Not a valid selection.');
             promptCustomer(res);
@@ -51,22 +60,28 @@ function promptCustomer(res) {
     })
 }
 
+// prompts customer for quantity to purchase
 function promptQuantity(product, productList) {
     inquirer.prompt({
         type: 'input',
         name: 'quantity',
         message: 'How many would you like to buy?'
     }).then(function (answer) {
+
+        // if there is enough inventory, products table is updated, total price is shown, and table/prompt are shown again
         if (product.stock_quantity >= answer.quantity) {
-            connection.query('UPDATE products SET stock_quantity="' + (product.stock_quantity - answer.quantity) + '", product_sales="' + (product.product_sales + answer.quantity * product.price) + '" WHERE item_id="' + product.item_id + '"', function () {
-                connection.query('UPDATE departments SET total_sales=total_sales+' + (answer.quantity * product.price) + ' WHERE department_name="' + product.department_name + '"', function () {
-                    console.log('Purchased ' + answer.quantity + ' of the item "' + product.product_name + '" for a total of $' + ((answer.quantity * product.price).toFixed(2)) + '.')});
-                createTable();
-            })
-        }
-        else if (product.stock_quantity < answer.quantity) {
-            console.log('There is only ' + product.stock_quantity + ' left in inventory to buy. Cannot complete purchase.');
-            promptCustomer(productList);
-        }
+                connection.query('UPDATE products SET stock_quantity="' + (product.stock_quantity - answer.quantity) + '", product_sales="' + (product.product_sales + answer.quantity * product.price) + '" WHERE item_id="' + product.item_id + '"', function () {
+                    connection.query('UPDATE departments SET total_sales=total_sales+' + (answer.quantity * product.price) + ' WHERE department_name="' + product.department_name + '"', function () {
+                        console.log('Purchased ' + answer.quantity + ' of the item "' + product.product_name + '" for a total of $' + ((answer.quantity * product.price).toFixed(2)) + '.')
+                    });
+                    createTable();
+                })
+            }
+
+            // if there is not enough inventory, message will show and table/prompt will show again
+            else if (product.stock_quantity < answer.quantity) {
+                console.log('There is only ' + product.stock_quantity + ' left in inventory to buy. Cannot complete purchase.');
+                promptCustomer(productList);
+            }
     });
 }
